43d550a54bab245828d1e73840c80cad9e76039f
jingzing <louiswang0898@gmail.com>
2016-12-15 16:59:45 +0800

    （29）Spring boot 文件上传（多文件上传）【从零开始学Spring Boot】

--- basis-spring-hello/src/main/java/tk/jingzing/Application.java
+++ basis-spring-hello/src/main/java/tk/jingzing/Application.java
@@ -2,6 +2,10 @@ package tk.jingzing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * http://www.tuicool.com/articles/6RzUZva
@@ -9,6 +13,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+
+    //对文件做一些限制是有必要的
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //// 设置文件大小限制 ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了;
+        factory.setMaxFileSize("128KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("256KB");
+        //Sets the directory location where files will be stored.
+        //factory.setLocation("路径地址");
+        return factory.createMultipartConfig();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("Let's inspect the beans provided by Spring Boot:");
