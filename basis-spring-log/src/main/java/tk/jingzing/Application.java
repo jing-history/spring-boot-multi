222987d2b3dbbd0b209144a93ef00c9bbadb1954
jingzing <louiswang0898@gmail.com>
2016-12-22 14:39:10 +0800

    Spring Boot 使用log

---
+++ basis-spring-log/src/main/java/tk/jingzing/Application.java
@@ -0,0 +1,16 @@
+package tk.jingzing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot日志升级篇—log4j【从零开始学Spring Boot】
+ * http://412887952-qq-com.iteye.com/blog/2306974
+ * Created by wangyunjing on 2016/12/7.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
