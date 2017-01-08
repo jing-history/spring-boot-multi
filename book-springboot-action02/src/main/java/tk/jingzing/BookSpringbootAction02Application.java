b102db0f8a2d01beadaa263c596c74e3a6ba887e
jingzing <louiswang0898@gmail.com>
2017-01-08 13:11:54 +0800

    Spring Boot book action test04

---
+++ book-springboot-action02/src/main/java/tk/jingzing/BookSpringbootAction02Application.java
@@ -0,0 +1,12 @@
+package tk.jingzing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSpringbootAction02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSpringbootAction02Application.class, args);
+	}
+}
---
+++ book-springboot-action02/src/test/java/tk/jingzing/ServerWebTests.java
@@ -0,0 +1,67 @@
+package tk.jingzing;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by wangyunjing on 2017/1/8.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(BookSpringbootAction02Application.class)
+@WebIntegrationTest(randomPort = true)
+public class ServerWebTests {
+
+    private static FirefoxDriver browser;
+
+    private static ChromeDriver chromeDriver;
+
+    @Value("${local.server.port}")
+    private int port;
+
+    @BeforeClass
+    public static void openBrowser() {
+        browser = new FirefoxDriver();
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void addBookToEmptyList() {
+        String baseUrl = "http://localhost:" + port;
+        browser.get(baseUrl);
+        assertEquals("You have no books in your book list",browser.findElementByTagName("div").getText());
+        browser.findElementByName("title")
+                .sendKeys("BOOK TITLE");
+        browser.findElementByName("author")
+                .sendKeys("BOOK AUTHOR");
+        browser.findElementByName("isbn")
+                .sendKeys("1234567890");
+        browser.findElementByName("description")
+                .sendKeys("DESCRIPTION");
+        browser.findElementByTagName("form")
+                .submit();
+
+        WebElement dl = browser.findElementByCssSelector("dt.bookHeadline");
+        assertEquals("BOOK TITLE by BOOK AUTHOR (ISBN: 1234567890)",
+                dl.getText());
+        WebElement dt = browser.findElementByCssSelector("dd.bookDescription");
+        assertEquals("DESCRIPTION", dt.getText());
+    }
+
+    @AfterClass
+    public static void closeBrowser() {
+        browser.quit();
+    }
+}
